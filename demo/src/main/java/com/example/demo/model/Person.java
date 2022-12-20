0ebc417a658897983295b1bc13bc53c08a294cff
daniele cocuzza <daniele.cocuzza@aitho.it>
2022-12-20 10:32:53 +0100

    integrazione DB

--- demo/src/main/java/com/example/demo/model/Person.java
+++ demo/src/main/java/com/example/demo/model/Person.java
@@ -1,22 +1,32 @@
 package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import org.aspectj.weaver.patterns.PerObject;
+import org.hibernate.annotations.GenericGenerator;
+
 
 import java.util.UUID;
 
+@Table(name = "person")
+@Entity
 
 public class Person {
-    private final UUID id;
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
 
-    //@NotBlank Non funziona
-    private final String name;
+    private UUID id;
 
-    public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name)
+    private String name;
+
+
+    /*public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name)
     {
         this.id = id;
         this.name = name;
 
-    }
+    }*/
 
     public UUID getId() {
         return id;
--- demo/src/main/java/com/example/demo/service/PersonService.java
+++ demo/src/main/java/com/example/demo/service/PersonService.java
@@ -1,12 +1,11 @@
 package com.example.demo.service;
 
-import com.example.demo.dao.PersonDao;
+import com.example.demo.dao.PersonRepository;
 import com.example.demo.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,35 +13,37 @@ import java.util.UUID;
 @Service
 public class PersonService {
 
-    private final PersonDao personDao;
+    @Autowired
+    private final PersonRepository personRepository;
 
     @Autowired
-    public PersonService(@Qualifier("fakeDao") PersonDao personDao)
+    public PersonService(PersonRepository personRepository)
     {
-        this.personDao = personDao;
+        this.personRepository = personRepository;
     }
-    public int addPerson(Person person)
+    public void addPerson(Person person)
     {
-        return personDao.insertPerson(person);
+        personRepository.save(person);
     }
 
     public List<Person> getAllPeople(){
-        return personDao.selectAllPeople();
+        return personRepository.findAll();
     }
 
     public Optional<Person> getPersonById(UUID id)
     {
-        return personDao.selectPersonById(id);
+        return personRepository.findById(id);
     }
 
-    public int deletePerson(UUID id)
+    public void deletePerson(UUID id)
     {
-        return personDao.deletePersonById(id);
+        personRepository.deleteById(id);
     }
 
-    public int updatePerson(UUID id, Person newPerson)
+    public void updatePerson(UUID id, Person newPerson)
     {
-        return personDao.updatePersonById(id, newPerson);
+        personRepository.deleteById(id);
+        personRepository.save(newPerson);
     }
 
 }
